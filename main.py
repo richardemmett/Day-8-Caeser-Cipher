659d94450e9eb5255317cb9bedab395777c9ea71
richardemmett <157606947+richardemmett@users.noreply.github.com>
2024-03-25 15:17:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char.isalpha():
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+import art
+print(art.logo)
+
+#Continue until break
+#Prompt for text, shift and direction
+while True: 
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+#Use modulus to deal with numbers greater than 26
+  caesar(start_text=text, shift_amount=shift % 26, cipher_direction=direction)
+
+#Ask the user if they want to restart the cipher program
+  restart = input("Type yes if you want to go again. Otherwise type no.\n")
+  if restart.lower() != "yes":
+    break
+    
